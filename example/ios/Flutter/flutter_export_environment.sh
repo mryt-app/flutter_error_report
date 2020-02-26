402997c7d955ca67c0c05379ffd6d54280bd8423
wangjf <wangjf01@missfresh.cn>
2020-02-26 14:47:51 +0800

    add introduction

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/wangjianfei/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wangjianfei/missfresh_flutter/flutter_error_report/example"
+export "FLUTTER_TARGET=/Users/wangjianfei/missfresh_flutter/flutter_error_report/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/wangjianfei/development/flutter/bin/cache/artifacts/engine/ios"
